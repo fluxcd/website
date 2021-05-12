b6c2d13ec5bf2ae29ac4b9680f78dbce51ad68f1
Daniel Holbach <daniel@weave.works>
2021-05-12 15:08:33 +0200

    make adopters page script more functional
    Signed-off-by: Daniel Holbach <daniel@weave.works>

--- hack/generate-adopters-pages.py
+++ hack/generate-adopters-pages.py
@@ -7,24 +7,13 @@ import sys
 
 # Workaround to make this work in Netlify...
 local_py_path = '/opt/buildhome/python3.7/lib/python3.7/site-packages/'
-if not local_py_path in sys.path:
+if local_py_path not in sys.path:
     sys.path.append(local_py_path)
 
 import yaml
 
-top_level_dir = os.path.realpath(
-    os.path.join(os.path.dirname(__file__), '..'))
-
-if os.getcwd() != top_level_dir:
-    print('Please run this script from top-level of the repository.')
-    sys.exit(1)
-
-adopters_dir = os.path.join(top_level_dir, 'adopters')
-content_dir = os.path.join(top_level_dir, 'content/en')
-adopters_page_fn = os.path.join(content_dir, 'adopters.md')
-
-f = open(adopters_page_fn, 'w')
-f.write('''
+def write_page_header(f):
+    f.write('''
 ---
 title: Flux Adopters
 type: page
@@ -39,11 +28,8 @@ To join this list, please follow [these instructions](https://github.com/fluxcd/
 
 ''')
 
-company_files = sorted(glob.glob(adopters_dir+'/*.yaml'))
-for yaml_fn in company_files:
+def write_section_header(yaml_fn, data, f):
     section_id = os.path.basename(yaml_fn).split('.yaml')[0][2:]
-    with open(yaml_fn, 'r') as File:
-        data = yaml.safe_load(File)
     section_title = data['adopters']['project']
     page_description = data['adopters']['description']
     f.write('''
@@ -53,48 +39,84 @@ for yaml_fn in company_files:
 
 '''.format(section_id, section_title, page_description))
 
-    companies = data['adopters']['companies']
-    how_many = len(companies)
-    companies = sorted(companies, key=lambda x: x['name'].lower())
+def fix_up_logo(adopters_dir, logo_entry):
+    if not logo_entry.startswith('https:'):
+        logo_fn = os.path.join(adopters_dir, logo_entry)
+        if not os.path.exists(logo_fn):
+            print('"{}" not found.'.format(logo_fn))
+            sys.exit(1)
+        logo_entry = '/img/' + logo_entry
+    return logo_entry
 
-    i = 0
-    for company in companies:
-        if i % 5 == 0:
-            f.write('''
-{{< cardpane >}}
+def write_card_text(f, company_name, company_url, company_logo):
+    card_text = '{{% card header="[' + \
+        company_name + '](' + \
+        company_url + ')" %}}\n'
+    card_text += '![' + company_name + '](' + \
+        company_logo + ')\n'
+    card_text += '{{% /card %}}\n'
+    f.write(card_text)
+
+
+def main():
+    top_level_dir = os.path.realpath(
+        os.path.join(os.path.dirname(__file__), '..'))
+
+    if os.getcwd() != top_level_dir:
+        print('Please run this script from top-level of the repository.')
+        sys.exit(1)
+
+    adopters_dir = os.path.join(top_level_dir, 'adopters')
+    content_dir = os.path.join(top_level_dir, 'content/en')
+    adopters_page_fn = os.path.join(content_dir, 'adopters.md')
+
+    f = open(adopters_page_fn, 'w')
+
+    write_page_header(f)
+
+    adopters_files = sorted(glob.glob(adopters_dir+'/*.yaml'))
+    for yaml_fn in adopters_files:
+        with open(yaml_fn, 'r') as File:
+            data = yaml.safe_load(File)
+        write_section_header(yaml_fn, data, f)
+
+        companies = data['adopters']['companies']
+        companies = sorted(companies, key=lambda x: x['name'].lower())
+
+        for company in companies:
+            i = companies.index(company)
+            if i % 5 == 0:
+                f.write('''{{< cardpane >}}
 ''')
-        if 'logo' not in company:
-            company['logo'] = 'logos/logo-generic.png'
-        if not company['logo'].startswith('https:'):
-            logo_fn = os.path.join(adopters_dir, company['logo'])
-            if not os.path.exists(logo_fn):
-                print('"{}" not found.'.format(logo_fn))
-                sys.exit(1)
-            company['logo'] = '/img/' + company['logo']
-        card_text = '{{% card header="[' + \
-            company['name'] + '](' + \
-            company['url'] + ')" %}}\n'
-        card_text += '![' + company['name'] + '](' + \
-            company['logo'] + ')\n'
-        card_text += '{{% /card %}}\n'
-        f.write(card_text)
+            if 'logo' not in company:
+                company['logo'] = 'logos/logo-generic.png'
+            company['logo'] = fix_up_logo(adopters_dir, company['logo'])
+            write_card_text(f, company['name'], company['url'], company['logo'])
 
-        if i % 5 == 4:
-            f.write('''
-{{< /cardpane >}}''')
-        i += 1
+            if i % 5 == 4:
+                f.write('''{{< /cardpane >}}
+''')
 
-    if i % 5 != 4:
-        f.write('''
-{{< /cardpane >}}''')
+        if i % 5 != 4:
+            f.write('''{{< /cardpane >}}
+''')
 
-new_logos_dir = os.path.join(top_level_dir, 'static/img/logos')
-if not os.path.exists(new_logos_dir):
-    os.makedirs(new_logos_dir)
+    new_logos_dir = os.path.join(top_level_dir, 'static/img/logos')
+    if not os.path.exists(new_logos_dir):
+        os.makedirs(new_logos_dir)
 
-for img in glob.glob(
-        os.path.join(adopters_dir, 'logos')+'/*'):
-    shutil.copyfile(
-        img,
-        os.path.join(new_logos_dir,
-                     os.path.basename(img)))
+    for img in glob.glob(
+            os.path.join(adopters_dir, 'logos')+'/*'):
+        shutil.copyfile(
+            img,
+            os.path.join(new_logos_dir,
+                         os.path.basename(img)))
+
+    f.close()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Aborted.", sys.stderr)
+        sys.exit(1)
