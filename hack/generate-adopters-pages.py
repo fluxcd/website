a97ace5dcc8b5e0cf887e553ca087324bbb2bf74
Daniel Holbach <daniel@weave.works>
2021-05-28 11:16:18 +0200

    add page description
    Signed-off-by: Daniel Holbach <daniel@weave.works>

--- hack/generate-adopters-pages.py
+++ hack/generate-adopters-pages.py
@@ -16,6 +16,10 @@ def write_page_header(f):
     f.write('''---
 title: Flux Adopters
 type: page
+description: >
+  The Flux community is immensely proud to have grown a lot over all the years. On this page you can see a selection of organisations who self-identified as using any of the Flux projects in production.
+
+  Thanks a lot for your trust, support and being part of our community!
 ---
 
 # Flux Adopters
