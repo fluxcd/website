7f3876e21ccbe95523aaf1b59e4719bf78f029bf
Daniel Holbach <daniel@weave.works>
2021-08-30 13:39:15 +0200

    Move to Focal Netlify image - includes python 3.8
    	Fixes: #488
    
    Signed-off-by: Daniel Holbach <daniel@weave.works>

--- hack/adopters.py
+++ hack/adopters.py
@@ -10,7 +10,7 @@ import shutil
 import sys
 
 # Workaround to make this work in Netlify...
-LOCAL_PY_PATH = '/opt/buildhome/python3.7/lib/python3.7/site-packages/'
+LOCAL_PY_PATH = '/opt/buildhome/python3.8/lib/python3.8/site-packages/'
 if LOCAL_PY_PATH not in sys.path:
     sys.path.append(LOCAL_PY_PATH)
 
--- hack/import-calendar.py
+++ hack/import-calendar.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from datetime import (date, timedelta)
 import glob
@@ -6,7 +6,7 @@ import os
 import sys
 
 # Workaround to make this work in Netlify...
-LOCAL_PY_PATH = '/opt/buildhome/python3.7/lib/python3.7/site-packages/'
+LOCAL_PY_PATH = '/opt/buildhome/python3.8/lib/python3.8/site-packages/'
 if LOCAL_PY_PATH not in sys.path:
     sys.path.append(LOCAL_PY_PATH)
 
